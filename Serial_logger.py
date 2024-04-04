0fd5dd47122b94c3e898f4ff57686dfafac10788
GilangPrakasa <prakasagilang08@gmail.com>
2024-04-04 19:35:18 +0700

    V1.0.0

--- Serial_logger.py
+++ Serial_logger.py
@@ -20,16 +20,22 @@ header_text_csv = [
         "INSPECTOR",
         "ID-COBOX",
         "MAC",
+        "RUN BUTTON",
+        "FLASH AVR",
+        "WDT",
+        "DISPLAY BUTTON",
         "BUZZER",
         "LED",
-        "BUTTON",
-        "FLASH-AVR",
         "RTC",
-        "NOR-FLASH",
+        "NOR FLASH",
         "RSSI",
         "PWM 30",
         "PWM 70",
         "PWM 100",
+        "RSSI VALUE",
+        "AVR STATE",
+        "STATUS",
+        "NOTE",
         "DURATION CHECKING"
         ]
 
@@ -41,6 +47,7 @@ class App(customtkinter.CTk):
         # configure window
         self.title("HARDWARE CHECKER")
         self.iconbitmap('assets/iconx.ico')
+        self.resizable(False,False)
 
         # configure grid layout
         self.grid_columnconfigure((0, 1), weight=1)
@@ -53,18 +60,18 @@ class App(customtkinter.CTk):
         
         # BAUDRATES
         self.label_baud = customtkinter.CTkLabel(self.FR1, text="BAUDRATE", font=customtkinter.CTkFont(size=15, weight="bold"))
-        self.label_baud.grid(row=5, column=0, padx=10, pady=10, sticky="W")
+        self.label_baud.grid(row=7, column=0, padx=10, pady=10, sticky="W")
 
         self.baudrates = ["115200","9600"]
         self.selected_baudrate = customtkinter.StringVar(self)
         self.selected_baudrate.set(self.baudrates[0])
         self.baudrate_option = customtkinter.CTkOptionMenu(self.FR1, width=150, values=self.baudrates, variable=self.selected_baudrate)
-        self.baudrate_option.grid(row=5, column=1, padx=10, pady=10)
+        self.baudrate_option.grid(row=7, column=1, padx=10, pady=10)
         # self.baudrate_option.set("BAUDRATE")
 
         # COM PORTS
         self.label_port = customtkinter.CTkLabel(self.FR1, text="PORT", font=customtkinter.CTkFont(size=15, weight="bold"))
-        self.label_port.grid(row=6, column=0, padx=10, pady=10, sticky="W")
+        self.label_port.grid(row=8, column=0, padx=10, pady=10, sticky="W")
 
         self.available_ports = self.get_serial_ports()
         self.selected_port = customtkinter.StringVar(self)
@@ -73,54 +80,70 @@ class App(customtkinter.CTk):
         else:
             self.selected_port.set("No ports available")
         self.port_option = customtkinter.CTkOptionMenu(self.FR1, width=150, values=self.available_ports, variable=self.selected_port)
-        self.port_option.grid(row=6, column=1, padx=10, pady=10)
+        self.port_option.grid(row=8, column=1, padx=10, pady=10)
         # self.port_option.set("COM PORT")
 
         # START BUTTON
         self.start_button = customtkinter.CTkButton(self.FR1, width=300, text="START APP", command=self.start_reading)
-        self.start_button.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
+        self.start_button.grid(row=9, column=0, columnspan=2, padx=10, pady=10)
 
         # RESTART BUTTON
         self.restart_button = customtkinter.CTkButton(self.FR1, width=300, text="START READING", command=self.stop_reading)
-        self.restart_button.grid(row=8, column=0, columnspan=2, padx=10, pady=10)
+        self.restart_button.grid(row=10, column=0, columnspan=2, padx=10, pady=10)
 
         # TEXT BOX
-        self.serial_data = customtkinter.CTkTextbox(self.FR1, width=600, height=300, corner_radius=10)
+        self.serial_data = customtkinter.CTkTextbox(self.FR1, width=600, height=400, corner_radius=10)
         self.serial_data.grid(row=0, column=0,columnspan=4,padx=10, pady=10, sticky="nsew")
 
 
         # CHECKLIST BUZZER LED BUTTON FLASH-AVR RTC NOR-FLASH MAC RSSI RUN30 RUN70 RUN100
+        self.button = customtkinter.StringVar(value="FAIL")
+        self.button = customtkinter.CTkCheckBox(self.FR1, text="RUN BUTTON", text_color="yellow", variable=self.button, onvalue="PASS", offvalue="FAIL")
+        self.button.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+
+        self.flash_avr = customtkinter.StringVar(value="FAIL")
+        self.flash_avr = customtkinter.CTkCheckBox(self.FR1, text="FlashAVR", text_color="yellow", variable=self.flash_avr, onvalue="PASS", offvalue="FAIL")
+        self.flash_avr.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+
+        self.wdt = customtkinter.StringVar(value="FAIL")
+        self.wdt = customtkinter.CTkCheckBox(self.FR1, text="WDT", text_color="yellow", variable=self.wdt, onvalue="PASS", offvalue="FAIL")
+        self.wdt.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+
+        self.disp_button = customtkinter.StringVar(value="FAIL")
+        self.disp_button = customtkinter.CTkCheckBox(self.FR1, text="DISP BUTTON", text_color="yellow", variable=self.disp_button, onvalue="PASS", offvalue="FAIL")
+        self.disp_button.grid(row=4, column=0, padx=10, pady=10, sticky="w")
+
         self.buzzer = customtkinter.StringVar(value="FAIL")
         self.buzzer = customtkinter.CTkCheckBox(self.FR1, text="BUZZER", text_color="yellow", variable=self.buzzer, onvalue="PASS", offvalue="FAIL")
-        self.buzzer.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+        self.buzzer.grid(row=5, column=0, padx=10, pady=10, sticky="w")
         
         self.led = customtkinter.StringVar(value="FAIL")
         self.led = customtkinter.CTkCheckBox(self.FR1, text="LED", text_color="yellow", variable=self.led, onvalue="PASS", offvalue="FAIL")
-        self.led.grid(row=2, column=0, padx=10, pady=10, sticky="w")
-
-        self.button = customtkinter.StringVar(value="FAIL")
-        self.button = customtkinter.CTkCheckBox(self.FR1, text="BUTTON", text_color="yellow", variable=self.button, onvalue="PASS", offvalue="FAIL")
-        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+        self.led.grid(row=6, column=0, padx=10, pady=10, sticky="w")
 
         self.rtc = customtkinter.StringVar(value="FAIL")
         self.rtc = customtkinter.CTkCheckBox(self.FR1, text="RTC", text_color="yellow", variable=self.rtc, onvalue="PASS", offvalue="FAIL")
-        self.rtc.grid(row=4, column=0, padx=10, pady=10, sticky="w")
+        self.rtc.grid(row=1, column=1, padx=10, pady=10, sticky="w")
 
         self.nor = customtkinter.StringVar(value="FAIL")
         self.nor = customtkinter.CTkCheckBox(self.FR1, text="NOR-FLASH", text_color="yellow", variable=self.nor, onvalue="PASS", offvalue="FAIL")
-        self.nor.grid(row=1, column=1, padx=10, pady=10, sticky="w")
+        self.nor.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+
+        self.rssi_c = customtkinter.StringVar(value="FAIL")
+        self.rssi_c = customtkinter.CTkCheckBox(self.FR1, text="RSSI VALUE", text_color="yellow", variable=self.rssi_c, onvalue="PASS", offvalue="FAIL")
+        self.rssi_c.grid(row=3, column=1, padx=10, pady=10, sticky="w")
 
         self.pwm30 = customtkinter.StringVar(value="FAIL")
         self.pwm30 = customtkinter.CTkCheckBox(self.FR1, text="RUN-30", text_color="yellow", variable=self.pwm30, onvalue="PASS", offvalue="FAIL")
-        self.pwm30.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+        self.pwm30.grid(row=4, column=1, padx=10, pady=10, sticky="w")
 
         self.pwm70 = customtkinter.StringVar(value="FAIL")
         self.pwm70 = customtkinter.CTkCheckBox(self.FR1, text="RUN-70", text_color="yellow", variable=self.pwm70, onvalue="PASS", offvalue="FAIL")
-        self.pwm70.grid(row=3, column=1, padx=10, pady=10, sticky="w")
+        self.pwm70.grid(row=5, column=1, padx=10, pady=10, sticky="w")
 
         self.pwm100 = customtkinter.StringVar(value="FAIL")
         self.pwm100 = customtkinter.CTkCheckBox(self.FR1, text="RUN-100", text_color="yellow", variable=self.pwm100, onvalue="PASS", offvalue="FAIL")
-        self.pwm100.grid(row=4, column=1, padx=10, pady=10, sticky="w")
+        self.pwm100.grid(row=6, column=1, padx=10, pady=10, sticky="w")
 
         self.label_avr = customtkinter.CTkLabel(self.FR1, text="AVR STATUS", font=customtkinter.CTkFont(size=15, weight="bold"))
         self.label_avr.grid(row=1, column=2, padx=10, pady=10, sticky="W")
@@ -154,33 +177,48 @@ class App(customtkinter.CTk):
 
         self.uid_value = customtkinter.StringVar()
         self.uid = customtkinter.CTkEntry(self.FR1, width=150, text_color="red", placeholder_text="UID", textvariable=self.uid_value)
-        self.uid.configure(state="disabled")
+        # self.uid.configure(state="disabled")
         self.uid.grid(row=4, column=3, padx=10, pady=10, sticky="W")
         self.uid_value.set("None")
 
         self.label_id = customtkinter.CTkLabel(self.FR1, text="ID COBOX", font=customtkinter.CTkFont(size=15, weight="bold"))
-        self.label_id.grid(row=6, column=2, padx=10, pady=10, sticky="W")
+        self.label_id.grid(row=5, column=2, padx=10, pady=10, sticky="W")
 
         self.id = customtkinter.CTkEntry(self.FR1, width=150, placeholder_text="ID COBOX")
-        self.id.grid(row=6, column=3, padx=10, pady=10, sticky="W")
+        self.id.grid(row=5, column=3, padx=10, pady=10, sticky="W")
 
         self.label_inspector = customtkinter.CTkLabel(self.FR1, text="INSPECTOR", font=customtkinter.CTkFont(size=15, weight="bold"))
-        self.label_inspector.grid(row=5, column=2, padx=10, pady=10, sticky="W")
+        self.label_inspector.grid(row=6, column=2, padx=10, pady=10, sticky="W")
 
         self.btn_text = customtkinter.StringVar()
         self.inspector_btn = customtkinter.CTkButton(self.FR1, width=150, textvariable=self.btn_text, command=self.set_inspector)
-        self.inspector_btn.grid(row=5, column=3, padx=10, pady=10, sticky="W")
+        self.inspector_btn.grid(row=6, column=3, padx=10, pady=10, sticky="W")
         self.inspector_btn.configure(fg_color="red")
         self.btn_text.set("Set Inspector")
 
+        self.label_status = customtkinter.CTkLabel(self.FR1, text="STATUS", font=customtkinter.CTkFont(size=15, weight="bold"))
+        self.label_status.grid(row=7, column=2, padx=10, pady=10, sticky="w")
+
+        self.status = ["None","PASS","REPAIR"]
+
+        self.status = customtkinter.CTkOptionMenu(self.FR1, width=150, values=["None","PASS","REPAIR"])
+        self.status.grid(row=7, column=3, padx=10, pady=10, sticky="W")
+        self.status.set("None")
+
+        self.label_note = customtkinter.CTkLabel(self.FR1, text="NOTE", font=customtkinter.CTkFont(size=15, weight="bold"))
+        self.label_note.grid(row=8, column=2, padx=10, pady=10, sticky="W")
+
+        self.note = customtkinter.CTkEntry(self.FR1, width=150, placeholder_text="NOTE")
+        self.note.grid(row=8, column=3, padx=10, pady=10, sticky="W")
+
         self.save = customtkinter.CTkButton(self.FR1, width=300, text="SAVE", command=self.save_result)
-        self.save.grid(row=7, column=2,columnspan=2, padx=10, pady=10)
+        self.save.grid(row=9, column=2,columnspan=2, padx=10, pady=10)
 
         self.exit = customtkinter.CTkButton(self.FR1, width=300, text="EXIT", command=self.on_closing)
-        self.exit.grid(row=8, column=2,columnspan=2, padx=10, pady=10)
+        self.exit.grid(row=10, column=2,columnspan=2, padx=10, pady=10)
 
         self.trademark = customtkinter.CTkLabel(self.FR1, text="CONTACT: mahesa.gilang@efishery.com",width=15,height=15, font=customtkinter.CTkFont(size=12,weight="normal"))
-        self.trademark.grid(row=9, column=0, columnspan=4, padx=5, pady=5, sticky="W")
+        self.trademark.grid(row=11, column=0, columnspan=4, padx=5, pady=5, sticky="W")
         
     def get_serial_ports(self):
         try:
@@ -197,6 +235,8 @@ class App(customtkinter.CTk):
 
     def stop_reading(self):
         if hasattr(self, 'serial_reader'):
+            self.uid_value.set("None")
+            self.uid.configure(text_color="red")
             self.serial_reader.stop_reading()
     
     def set_inspector(self):
@@ -231,26 +271,35 @@ class App(customtkinter.CTk):
                 f"{self.user_input}",
                 f"{self.id.get()}",
                 f"{self.mac.get()}",
+                f"{self.button.get()}",
+                f"{self.flash_avr.get()}",
+                f"{self.wdt.get()}",
+                f"{self.disp_button.get()}",
                 f"{self.buzzer.get()}",
                 f"{self.led.get()}",
-                f"{self.button.get()}",
-                f"{self.avr.get()}",
                 f"{self.rtc.get()}",
                 f"{self.nor.get()}",
-                f"{self.rssi.get()}",
+                f"{self.rssi_c.get()}",
                 f"{self.pwm30.get()}",
                 f"{self.pwm70.get()}",
                 f"{self.pwm100.get()}",
+                f"{self.rssi_value.get()}",
+                f"{self.avr.get()}",
+                f"{self.status.get()}",
+                f"{self.note.get()}",
                 f"{stopwatch}"
+                # f"stopwatch"
                 ]
-            if 'None' in data_text or 'FAIL' in data_text:
+            if 'Jancuk' in data_text or 'Jancuk2' in data_text:
                 CTkMessagebox(title="Message",width=150,height=100,message="Something was empty & fail",icon="assets/icon.png", option_1="ok")
                 # return False
-            else: 
-                self.write_uid()
+            else:
+                if self.uid.get() == self.new_id: 
+                    self.write_uid()
+                    self.serial_data.insert("end", f"UID APPEND {self.new_id}\n")
+                    pass
                 self.serial_data.insert("end", f"\n")
                 self.serial_data.insert("end", f"ELAPSED TIME :{stopwatch} SECONDS\n")
-                self.serial_data.insert("end", f"UID APPEND {self.new_id}\n")
                 header_text = header_text_csv
                 check_file = os.path.isfile(f"DATA_QC_FIRMWARE_{date_month}.csv")
                 csv_file_path = f"DATA_QC_FIRMWARE_{date_month}.csv"
@@ -261,6 +310,18 @@ class App(customtkinter.CTk):
                 with open(csv_file_path, mode='a', newline='') as csv_file:
                     csv_writer = csv.writer(csv_file)
                     csv_writer.writerow(data_text)
+                    self.button.deselect()
+                    self.flash_avr.deselect()
+                    self.wdt.deselect()
+                    self.disp_button.deselect()
+                    self.buzzer.deselect()
+                    self.led.deselect()
+                    self.rtc.deselect()
+                    self.nor.deselect()
+                    self.rssi_c.deselect()
+                    self.pwm30.deselect()
+                    self.pwm70.deselect()
+                    self.pwm100.deselect()
                     self.avr_value.set("None")
                     self.avr.configure(text_color="red")
                     self.mac_value.set("None")
@@ -269,18 +330,13 @@ class App(customtkinter.CTk):
                     self.rssi.configure(text_color="red")
                     self.uid_value.set("None")
                     self.uid.configure(text_color="red")
-                    self.id.delete(0,20)
-                    self.buzzer.deselect()
-                    self.led.deselect()
-                    self.button.deselect()
-                    self.rtc.deselect()
-                    self.nor.deselect()
-                    self.pwm30.deselect()
-                    self.pwm70.deselect()
-                    self.pwm100.deselect()
-                # self.btn_text.set("Set Inspector")
+                    self.id.delete(0,20)  
+                    # self.btn_text.set("Set Inspector")
+                    self.note.delete(0,50)
+                    self.status.set("None")
         except:
-            CTkMessagebox(title="Message",width=150,height=100,message="Data tidak lengkap",icon="assets/icon.png", option_1="ok")
+            CTkMessagebox(title="Message",width=150,height=100,message="GALAT",icon="assets/icon.png", option_1="ok")
+            # pass
 
     def on_closing(self):
         if hasattr(self, 'serial_conn'):
@@ -356,10 +412,14 @@ class SerialReader:
                         self.app.rssi_value.set("Failed")
                         self.app.rssi.configure(text_color="red")
                 if "TEST FINISHED" in text:
-                    self.unique_id =  self.app.generate_id()
-                    print("Generated ID:",  self.unique_id)
-                    self.app.uid_value.set(f"{ self.unique_id}")
-                    self.app.uid.configure(text_color="yellow")
+                    self.check_uid = self.app.uid.get()
+                    if self.check_uid == "None":
+                        self.unique_id =  self.app.generate_id()
+                        print("Generated ID:",  self.unique_id)
+                        self.app.uid_value.set(f"{ self.unique_id}")
+                        self.app.uid.configure(text_color="yellow")
+                    else:
+                        print("ID: CHECKED",  self.check_uid)
                                   
     def stop_reading(self):
         if self.is_reading:
